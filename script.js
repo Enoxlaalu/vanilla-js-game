e274e24b5cb9f1f9586dff7720544792e2ab1ed0
Enoxlaalu <enoxlaalu@gmail.com>
2023-04-29 20:59:04 +0300

    added canvas with background parallax

--- script.js
+++ script.js
@@ -1,68 +1,133 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-
-const width = canvas.width = 600
-const height = canvas.height = 600
-const spriteWidth = 575
-const spriteHeight = 523
-
-let frameX = 0
 let gameFrame = 0
 let staggerFrames = 5
-let playerState = 'sit'
+let playerState = 'idle'
+let gameSpeed = 5
 
-const image = new Image()
+function prepareImage(path) {
+    const image = new Image()
 
-image.src = 'shadow_dog.png'
+    image.src = `assets/img/${path}.png`
 
-const animationStates = {
-    idle: 7,
-    jump: 7,
-    fall: 7,
-    run: 9,
-    dizzy: 11,
-    sit: 5,
-    roll: 7,
-    bite: 7,
-    ko: 12,
-    getHit: 4
+    return image
 }
 
-const select = document.getElementById('animationsSelect')
-const states = Object.keys(animationStates)
+function renderDog() {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
 
-select.addEventListener('change', (e) => {
-    playerState = e.target.value
-})
+    const width = canvas.width = 600
+    const height = canvas.height = 600
+    const spriteWidth = 575
+    const spriteHeight = 523
 
-const selectOptions = []
+    const animationStates = {
+        idle: 7,
+        jump: 7,
+        fall: 7,
+        run: 9,
+        dizzy: 11,
+        sit: 5,
+        roll: 7,
+        bite: 7,
+        ko: 12,
+        getHit: 4
+    }
 
-states.forEach(state => {
-    const option = document.createElement("option")
-    option.value = state
-    option.innerText = state
-    selectOptions.push(option)
-})
+    const select = document.getElementById('animationsSelect')
+    const states = Object.keys(animationStates)
 
-select.append(...selectOptions)
+    select.addEventListener('change', (e) => {
+        playerState = e.target.value
+    })
 
-function animate() {
-    ctx.clearRect(0, 0, width, height)
+    const selectOptions = []
 
-    let position = Math.floor(gameFrame/staggerFrames) % (animationStates[playerState] - 1)
-    const frameY = states.indexOf(playerState)
+    states.forEach(state => {
+        const option = document.createElement("option")
+        option.value = state
+        option.innerText = state
+        selectOptions.push(option)
+    })
 
-    ctx.drawImage(image, position * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    select.append(...selectOptions)
 
-    // if (gameFrame % staggerFrames == 0) {
-    //     if (frameX < 6) {
-    //         frameX++
-    //     } else frameX = 0
-    // }
+    const image = prepareImage('shadow_dog')
 
-    gameFrame++
+    function animate() {
+        ctx.clearRect(0, 0, width, height)
 
-    requestAnimationFrame(animate)
+        let position = Math.floor(gameFrame/staggerFrames) % (animationStates[playerState] - 1)
+        const frameY = states.indexOf(playerState)
+
+        ctx.drawImage(image, position * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+
+        gameFrame++
+
+        requestAnimationFrame(animate)
+    }
+
+    animate()
 }
 
-animate()
+function renderBg() {
+    const canvas = document.getElementById('bgCanvas')
+    const ctx = canvas.getContext('2d')
+
+    const width = canvas.width = 800
+    const height = canvas.height = 700
+
+    const gameSpeedValue = document.getElementById('gameSpeedValue')
+    const rangeInput = document.getElementById('rangeInput')
+
+    rangeInput.value = gameSpeed
+    gameSpeedValue.innerText = gameSpeed
+
+    rangeInput.addEventListener('change', (e) => {
+        const value = e.target.value
+
+        gameSpeed = value
+        gameSpeedValue.innerText = value
+    })
+
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0
+            this.y = 0
+            this.width = 2400
+            this.height = 700
+            this.image = image
+            this.speedModifier = speedModifier
+            this.speed = gameSpeed * this.speedModifier
+        }
+
+        draw() {
+            this.speed = gameSpeed * this.speedModifier
+
+            ctx.drawImage(this.image, this.x, 0)
+            ctx.drawImage(this.image, this.x + this.width, 0)
+
+            if (this.x <= -this.width) this.x = 0
+
+            this.x-= this.speed
+        }
+    }
+
+    const layers = Array(5).fill('').map((l, index) => {
+        const image = prepareImage(`bg/layer-${index+1}`)
+        return new Layer(image, index + 1)
+    })
+
+    function animate() {
+        ctx.clearRect(0, 0, width, height)
+
+        layers.forEach((l) => {
+            l.draw()
+        })
+
+        requestAnimationFrame(animate)
+    }
+    animate()
+}
+
+renderDog()
+renderBg()
